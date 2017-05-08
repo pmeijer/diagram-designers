c9d9adebd4fb396a957af74aadf1521707539d0f
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2017-05-08 14:34:32 -0500

    Add link to bip's svgs.

--- config/config.default.js
+++ config/config.default.js
@@ -4,6 +4,6 @@ var config = require('./config.webgme'),
     validateConfig = require('webgme/config/validator');
 
 config.mongo.uri = 'mongodb://127.0.0.1:27017/bip';
-
+config.visualization.svgDirs.push('./node_modules/webgme-bip/src/svgs');
 validateConfig(config);
 module.exports = config;
